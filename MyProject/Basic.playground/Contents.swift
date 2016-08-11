257857c38a9e267573d2d1a8de9a7b7c7b468670
zcwlwen <zcw_lwen@sina.cn>
2016-08-11 13:44:13 +0800

    学习Swift基础 添加教程中的代码

---
+++ MyProject/Basic.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Int 有负数UInt 没有负数
+var imInt: Int = 10
+
+UInt.max
+
+UInt.min
+
+let a = 1000_000
+
+
