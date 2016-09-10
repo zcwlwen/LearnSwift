6592b2fabf2eb244e29340770c2e827c53eb2db5
zcwlwen <zcw_lwen@sina.cn>
2016-09-10 16:50:10 +0800

    Optional 可选型

--- MyProject/Optional.playground/Contents.swift
+++ MyProject/Optional.playground/Contents.swift
@@ -36,3 +36,42 @@ if let strOp = strOp,errorMessage = errorMessage where strOp == "404"{
 var hello:String!
 
 
+// Optional chaining
+if let errorMessage = errorMessage{
+    errorMessage.uppercaseString
+}
+
+errorMessage?.uppercaseString
+
+var uppercaseErrorMessage = errorMessage?.uppercaseString
+
+if let errorMessage = errorMessage?.uppercaseString{
+    errorMessage
+}
+
+// Optional chaining 也可以使用!
+let uppercaseErrorMessage2 = errorMessage!.uppercaseString
+
+
+var errorMessage1: String? = nil
+
+let message: String
+if let errorMessage1 = errorMessage1{
+    message = errorMessage1
+}
+else{
+    message = "No error"
+}
+
+// 使用三目运算符
+let message2 = errorMessage1 == nil ? "No error" : errorMessage1!
+// 注意: 此时使用errorMessage需要强制解包.以保证message2是一个String，而非String?
+// 三目运算符?:的实质就是一个if else
+
+
+// 使用 ??
+//这里message3就是要取errorMessage1得值，取不到赋值No error
+let message3 = errorMessage1 ?? "No error"
+// 注意: 此时使用errorMessage不需要强制解包.
+// ??符号已经保证了访问到errorMessage时, errorMessage不是nil
+// Swift将为我们自动解包, 保证message2永远是一个String, 而不是String?
